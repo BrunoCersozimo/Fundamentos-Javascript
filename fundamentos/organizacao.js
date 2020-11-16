3c60af0e0706c46d18a7978ecd521a25021b2bc2
Bruno Cersozimo <71533963+BrunoCersozimo@users.noreply.github.com>
2020-11-16 15:41:53 -0300

    Confirmar

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,12 @@
+//Organização do código em javascript pode ser em:
+//Sentença de código
+console.log("Sentença de código")
+
+//Em bloco de código
+{
+    {
+        console.log("Olá")
+        console.log('Mundo!')
+    }
+}
+ 
---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,13 @@
+var a = 3
+let b = 4
+
+var a = 30  // dessa forma
+b = 40
+
+console.log(a, b)
+
+const c = 5
+//c = 50
+console.log(c)
+
+// No código quanto mais const tiver melhor será o dódigo. Usar const para valores que não serão alterados posteriormente no programa
