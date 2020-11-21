98b4268a2c6e28ca80f740de3a8dd61e407ea867
Bruno Cersozimo <71533963+BrunoCersozimo@users.noreply.github.com>
2020-11-21 11:53:11 -0300

    Enviar

---
+++ fundamentos/funcaoEmQuaseTudo.js
@@ -0,0 +1,4 @@
+console.log(typeof Object) // o tipo de dados de um Object em javascript é uma function
+
+class Produto {}
+console.log(typeof Produto) // class uma estrutura nova no javascript, que está em vigor a partir do ecmascript 2015 também é uma function
