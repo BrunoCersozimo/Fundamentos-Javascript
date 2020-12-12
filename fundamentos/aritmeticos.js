002675defbf5007618a7bcd0a079cce67ac0c904
Bruno Cersozimo <71533963+BrunoCersozimo@users.noreply.github.com>
2020-12-12 13:50:15 -0300

    Operadores: Aritméticos

---
+++ fundamentos/aritmeticos.js
@@ -0,0 +1,8 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2 // Operação de modulo -  resto da divisão. Ele pega o número passado e divide por 2 e o resto da divisão será o resultado. Isso mostra o resultado se é par ou ímpar 
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
