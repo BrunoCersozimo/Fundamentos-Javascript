7af9938ec9d489b8fa1bdcc9207230dab7411ce5
Bruno Cersozimo <71533963+BrunoCersozimo@users.noreply.github.com>
2020-12-17 18:58:16 -0300

    Anotações

--- fundamentos/ternario.js
+++ fundamentos/ternario.js
@@ -1,5 +1,12 @@
-const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado' 
+// A estrutura de um operador ternário, é uma versão compacta da estrutura if/else
 
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado' 
+/*
+resultado é o nome da variável,
+nota => nota => 7 É a condição,
+? Aprovado Se o valor for verdadeiro,
+: Reprovado se for falso
+*/
 
 console.log(resultado(7.1))
 console.log(resultado(6.7))
