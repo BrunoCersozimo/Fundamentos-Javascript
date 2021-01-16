8bec727d305904547c96ac1247fc3a73e67b9e4c
Bruno Cersozimo <71533963+BrunoCersozimo@users.noreply.github.com>
2021-01-16 11:23:45 -0300

    CallBack01

--- funcao/callback01.js
+++ funcao/callback01.js
@@ -1,5 +1,7 @@
-const Fabricante = ["Mercedes", "Audi", "BMW"]
-
+const fabricantes = ["Mercedes", "Audi", "BMW"]
 function imprimir(nome, indice){
     console.log(`${indice + 1}. ${nome}`)
 }
+
+fabricantes.forEach(imprimir)
+fabricantes.forEach(fabricante => console.log(fabricante))
