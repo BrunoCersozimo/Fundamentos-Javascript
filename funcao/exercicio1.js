465db2ef4f5612f87b637ff1bffacc3156a8405d
Bruno Cersozimo <71533963+BrunoCersozimo@users.noreply.github.com>
2021-01-29 19:49:55 -0300

    exercício 1

---
+++ funcao/exercicio1.js
@@ -0,0 +1,12 @@
+// Exercício 1
+
+function exer1(a, b){
+    resultado1 = a + b
+    resultado2 = a - b 
+    resultado3 = a * b
+    resultado4 = a / b
+    return console.log(resultado1, resultado2, resultado3, resultado4)
+}
+
+exer1(4, 2)
+
